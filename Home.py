dab5b8e1be64128e8f796639eeb4ee81a3da00e6
MatFrancois <matthieu.francois@yahoo.fr>
2022-06-14 17:23:43 +0200

    home

--- Home.py
+++ Home.py
@@ -10,5 +10,15 @@ st.set_page_config(
 st.title("Fenêtre d'Overton et NLP")
 st.sidebar.image('imgs/logo.png')
 '''
-Please find here 1 form to understand better how people are feeling about the ecology in general.
+*Please find here 1 form to understand better how people are feeling about the ecology in general.*
+
+What can I find here?
+- A [Form page](https://share.streamlit.io/matfrancois/4subjectsform/main/Home.py/Form) with ecology oriented questions.
+- An [Annotation page](https://share.streamlit.io/matfrancois/4subjectsform/main/Home.py/Annotation) to see how public speechs can be considered by our model 
+
+
+What is the Overton Window?
+
+Add graphics
+
 '''
